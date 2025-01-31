86478a7115e0559436f65c08acb63d2eda5eb66d
Jaroslav Nemec <j.nemecc9@gmail.com>
2025-01-31 12:52:49 +0100

    change tech stack desc.

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -4,7 +4,7 @@
         <h2>Aplikace vyžívající VUE.js a FastAPI</h2>
         <h3>Autor: J.Němec</h3>
         <p>Tato aplikace byla vytvořena dne 30.1.2025 jako vzorová práce</p>
-        <p>Kompletní tech stack: Python/FastAPI, Celery, Redis, SQLite, VUE.js, NGINX, Docker</p>
+        <p>Kompletní tech stack: Python/FastAPI, Celery, Redis, PostgreSQL, VUE.js, NGINX, Docker</p>
         <h3>2025</h3>
     </div>
 </template>
